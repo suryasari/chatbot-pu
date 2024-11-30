9ef44d5e81ff02c6fcdd3414596fde9915eabb34
Suryasari Siregar <suryasari.siregar@kemlu.go.id>
2024-11-30 21:32:39 +0800

    Firt Init Git Repo

---
+++ waWeb.js
@@ -0,0 +1,36 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+//tambah async suapaya bisa await
+client.on('message', async msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+    if (msg.body.startsWith('!echo ')) {
+        // Replies with the same message
+        msg.reply(msg.body.slice(6));
+    }
+   if (msg.body === '!mediainfo' && msg.hasMedia) 
+    {
+        const attachmentData = await msg.downloadMedia();
+        msg.reply(`
+            *Media info*
+            MimeType: ${attachmentData.mimetype}
+            Filename: ${attachmentData.filename}
+            Data (length): ${attachmentData.data.length}
+        `);
+   }
+
+});
+
+client.initialize();
